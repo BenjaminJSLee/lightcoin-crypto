85c2c215e811b5262fd0e50174aa3f5efd06e8da
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-06 21:13:34 +0000

    added functionality to the Withdraw class, and also added the Account, Transaction, and Deposit classes

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-class Withdrawal {
+  get balance() {
+    return this.transactions.reduce((acc,curTrans) => acc + curTrans.value , 0);
+  }
 
-  constructor(amount) {
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
+  isAllowed() {
+    return this.account.balance + this.value >= 0;
+  }
   commit() {
-    balance -= this.amount;
+    let allow = this.isAllowed();
+    if (allow) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    }
+    return allow;
   }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -(this.amount);
+  }
 }
 
 
@@ -18,12 +50,19 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(100.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+
+console.log('Balance:', myAccount.balance);
